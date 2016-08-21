be71e8eceac2adf666e7bf3e048708cbbb2c3e71
karim Tarek <karimmtarek@gmail.com>
2016-08-21 13:45:52 -0400

    Add dependencies

--- lib/newsfeeder.rb
+++ lib/newsfeeder.rb
@@ -1,4 +1,10 @@
-require "newsfeeder/version"
+require_relative "newsfeeder/version"
+
+require 'mechanize'
+require 'httparty'
+require 'zip'
+require 'redis'
+require 'progress_bar'
 
 module Newsfeeder
   # Your code goes here...
